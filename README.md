0ecbe10c71b15f37082a6cf69e7d87887461d995
DESKTOP-52Q97OD\rodney <rodney.addo@gmail.com>
2017-09-20 08:38:43 +0200

    new update using npm express to host website, and sharing work in progress using npm localtunnel

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # 4surefonts.github.io
 create a test app to test variety of mobile fonts on devices
 * This is a new test on readme
+
+start express 
+node index.js
+node buildScripts/scrServer.js
+
+host website for public  
+lt --port 3000
---
+++ buildScripts/scrServer.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var open = require('open');
+
+var port = 3000;
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
+});
